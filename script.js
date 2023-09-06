853cf3e62bc63fe363779ae5560501052b1e341b
Kisieles <106324737+Kisieles@users.noreply.github.com>
2023-09-06 20:24:15 +0200

    added game rules

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-function getComputerChoice() {
+let computerScore = 0;
+let playerScore = 0;
+
+function getComputerSelection() {
   let choiceNum = Math.floor(Math.random() * 3);
   if (choiceNum == 0) {
     return "rock";
@@ -6,5 +9,27 @@ function getComputerChoice() {
     return "paper";
   } else if (choiceNum == 2) {
     return "scissors";
+  }
+}
+
+function getPlayerSelection() {
+  const choice = prompt("What is your choice?");
+  return choice;
+}
+
+let computerSelection = getComputerSelection();
+let playerSelection = getPlayerSelection();
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "Tie";
+  } else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    return "player wins";
+  } else {
+    return "computer wins";
   }
 }
